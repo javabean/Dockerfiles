3260020bc21ccf3159bc4fef52209bf73dc1aba4
Cédrik LIME <REMOVE_ME+github@cedrik.fr>
2016-07-17 22:28:07 +0200

    Switch from runit to tinit: less processes on host (part 2)

---
+++ apache-base/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# this if will check if the first argument is a flag
+# but only works if all arguments require a hyphenated flag
+# -v; -SL; -f arg; etc will work, but not arg1 arg2
+if [ "${1:0:1}" = '-' ]; then
+    set -- apache2ctl "$@"
+fi
+
+# check for the expected command
+if [ "$1" = 'apache2ctl' -o "$1" = '/usr/sbin/apache2ctl' ]; then
+	# copy backup of conf dirs if mounted volume is empty
+	/usr/local/bin/restore_conf.sh
+
+	rm -f /usr/local/apache2/logs/httpd.pid /var/run/apache2/apache2.pid
+	#exec gosu www-data /usr/sbin/apache2 -DFOREGROUND -k start
+	exec "$@" -D FOREGROUND -k start
+fi
+
+# else default to run whatever the user wanted like "bash"
+exec "$@"
+
