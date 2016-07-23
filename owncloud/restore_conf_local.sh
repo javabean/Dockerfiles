05e092684d92398aab13dc97b1daaee424dbc4da
Cédrik LIME <REMOVE_ME+github@cedrik.fr>
2016-07-23 22:47:48 +0200

    Fix ownCloud service discovery when behind a SSL reverse proxy
    /.well-known redirecting being handled by httpd (not PHP), we need to persuade httpd that it is serving secure pages when behind a reverse http proxy that terminates SSL

--- owncloud/restore_conf_local.sh
+++ owncloud/restore_conf_local.sh
@@ -12,3 +12,7 @@ fi
 if ! ls -A /var/www/owncloud/apps/* > /dev/null 2>&1; then
 	tar xzf /owncloud-apps.tgz -C /var/www/owncloud
 fi
+
+if [ ! -z "${SERVER_NAME}" ]; then
+	sed -i'' -e "s%^\(\s*\)#*\(.*\)__SERVER_NAME__\(.*\)$%\1\2${SERVER_NAME}\3%g" /etc/apache2/conf-enabled/owncloud.conf
+fi
