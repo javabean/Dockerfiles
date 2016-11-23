df6cab8a9cace2d8961c746776e034e8cb422dae
Cédrik LIME <REMOVE_ME+github@cedrik.fr>
2016-11-23 22:04:07 +0100

    Fix infrastructure changes not propagating in config

--- dnsmasq/docker-entrypoint.sh
+++ dnsmasq/docker-entrypoint.sh
@@ -13,13 +13,14 @@ if [ "$1" = 'dnsmasq' -o "$1" = '/usr/sbin/dnsmasq' ]; then
 	# copy backup of conf dirs if mounted volume is empty
 	/usr/local/bin/restore_conf.sh
 	DNSMASQ_ARGS="-k -u dnsmasq --no-dhcp-interface=eth0 --conntrack --conf-file=/etc/dnsmasq.conf --conf-dir=/etc/dnsmasq.d,*.conf,.dpkg-dist,.dpkg-old,.dpkg-new --domain-needed --dns-loop-detect --stop-dns-rebind --rebind-localhost-ok"
-	CONSUL_TEMPLATE_OPTS="-template=/opt/dnsmasq/consul.json.ctmpl:/usr/local/etc/consul.d/dnsmasq.json"
+	CONSUL_TEMPLATE_OPTS=
 	if [ -f /opt/dnsmasq/resolv.conf.ctmpl ]; then
 		CONSUL_TEMPLATE_OPTS="$CONSUL_TEMPLATE_OPTS -template=/opt/dnsmasq/resolv.conf.ctmpl:/opt/dnsmasq/resolv.conf"
 	fi
+	export CONSUL_TEMPLATE_OPTS
 	# wait for local Consul agent
 	#wait_for.sh -n "Consul" -- curl -fsS -o /dev/null http://127.0.0.1:8500/
-	#exec consul-template.sh -exec="$@ ${DNSMASQ_ARGS} ${DNSMASQ_EXTRA_OPTS}" -exec-kill-timeout="5s"
+	#exec consul-template.sh -exec="$@ ${DNSMASQ_ARGS} ${DNSMASQ_EXTRA_OPTS}" -exec-kill-timeout="5s" -template="/opt/dnsmasq/consul.json.ctmpl:/usr/local/etc/consul.d/dnsmasq.json:consul reload"
 	exec "$@" ${DNSMASQ_ARGS} ${DNSMASQ_EXTRA_OPTS}
 fi
 
