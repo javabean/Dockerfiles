7043b9b40c43d8d04d288874e4e37ba16875fc29
Cédrik LIME <REMOVE_ME+github@cedrik.fr>
2017-03-19 14:41:44 +0100

    Fix process check
    Who the heck invented process name truncation?

--- transmission/consul-healthcheck.sh
+++ transmission/consul-healthcheck.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # transmission-daemon process
-num_processes=`pgrep -cx transmission-daemon`
+# pgrep: The process name used for matching is limited to the 15 characters present in the output of /proc/pid/stat
+num_processes=`pgrep -cx transmission-da`
 if [ "$num_processes" -eq 0 ]; then
 	echo "No transmission-daemon process!"
 	exit 2
